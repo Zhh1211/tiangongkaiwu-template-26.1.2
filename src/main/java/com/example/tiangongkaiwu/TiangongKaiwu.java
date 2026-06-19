776a9efef49fbc5601fc355797860984b50d2e3e
Zhh1211 <13674995025@163.com>
2026-06-19 23:15:03 +0800

    新增物品“水稻种子"

--- src/main/java/com/example/tiangongkaiwu/TiangongKaiwu.java
+++ src/main/java/com/example/tiangongkaiwu/TiangongKaiwu.java
@@ -38,33 +38,52 @@ public class TiangongKaiwu {
     public static final String MODID = "tiangongkaiwu";
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "tiangongkaiwu" namespace
+    // Create a Deferred Register to hold Blocks which will all be registered under
+    // the "tiangongkaiwu" namespace
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "tiangongkaiwu" namespace
+    // Create a Deferred Register to hold Items which will all be registered under
+    // the "tiangongkaiwu" namespace
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "tiangongkaiwu" namespace
-    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be
+    // registered under the "tiangongkaiwu" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister
+            .create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    // Creates a new Block with the id "tiangongkaiwu:example_block", combining the namespace and path
-    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", p -> p.mapColor(MapColor.STONE));
-    // Creates a new BlockItem with the id "tiangongkaiwu:example_block", combining the namespace and path
-    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
+    // Creates a new Block with the id "tiangongkaiwu:example_block", combining the
+    // namespace and path
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block",
+            p -> p.mapColor(MapColor.STONE));
+    // Creates a new BlockItem with the id "tiangongkaiwu:example_block", combining
+    // the namespace and path
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block",
+            EXAMPLE_BLOCK);
 
-    // Creates a new food item with the id "tiangongkaiwu:example_id", nutrition 1 and saturation 2
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", p -> p.food(new FoodProperties.Builder()
-            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+    // Creates a new food item with the id "tiangongkaiwu:example_id", nutrition 1
+    // and saturation 2
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item",
+            p -> p.food(new FoodProperties.Builder()
+                    .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
 
-    // Creates a creative tab with the id "tiangongkaiwu:example_tab" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
-            .title(Component.translatable("itemGroup.tiangongkaiwu")) //The language key for the title of your CreativeModeTab
-            .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
-            .displayItems((parameters, output) -> {
-                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
-            }).build());
+    // 水稻种子（新增）
+    public static final DeferredItem<Item> RICE_SEED = ITEMS.registerSimpleItem("rice_seed");
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    // Creates a creative tab with the id "tiangongkaiwu:example_tab" for the
+    // example item, that is placed after the combat tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS
+            .register("example_tab", () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.tiangongkaiwu")) // The language key for the title of your
+                                                                              // CreativeModeTab
+                    .withTabsBefore(CreativeModeTabs.COMBAT)
+                    .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+                    .displayItems((parameters, output) -> {
+                        output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this
+                                                           // method is preferred over the event
+                    }).build());
+
+    // The constructor for the mod class is the first code that is run when your mod
+    // is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and
+    // pass them in automatically.
     public TiangongKaiwu(IEventBus modEventBus, ModContainer modContainer) {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
@@ -77,14 +96,17 @@ public class TiangongKaiwu {
         CREATIVE_MODE_TABS.register(modEventBus);
 
         // Register ourselves for server and other game events we are interested in.
-        // Note that this is necessary if and only if we want *this* class (TiangongKaiwu) to respond directly to events.
-        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        // Note that this is necessary if and only if we want *this* class
+        // (TiangongKaiwu) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in
+        // this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        // Register our mod's ModConfigSpec so that FML can create and load the config
+        // file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
@@ -101,10 +123,15 @@ public class TiangongKaiwu {
         Config.ITEM_STRINGS.get().forEach((item) -> LOGGER.info("ITEM >> {}", item));
     }
 
-    // Add the example block item to the building blocks tab
+    // Add the example block item to the building blocks tab, and the rice seed to
+    // ingredients tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
             event.accept(EXAMPLE_BLOCK_ITEM);
+        }
+        // 水稻种子加入“原材料”标签
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(RICE_SEED);
         }
     }
 
